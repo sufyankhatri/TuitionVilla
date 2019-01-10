0855e0ad6afd32440bfabf00fb82885ec0db9514
sufyankhatri <43412206+sufyankhatri@users.noreply.github.com>
2019-01-10 18:14:30 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import {Text} from 'react-native';
+import Routes from './src/Components/Routes';
+export default class App extends Component{
+
+
+  render(){
+    return(
+      <Routes/>
+    );
+  }
+}
