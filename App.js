9172c4c7e721b8da43003215b8feaa6fb058a58c
sufyankhatri <43412206+sufyankhatri@users.noreply.github.com>
2019-01-15 22:21:22 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React,{Component} from 'react';
-import {Text} from 'react-native';
+import React, { Component } from 'react';
 import Routes from './src/Components/Routes';
-export default class App extends Component{
-
-
-  render(){
-    return(
-      <Routes/>
+import ReduxThunk from 'redux-thunk';
+import firebase from '@firebase/app';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './src/reducers';
+import WelcomeScreen from './src/Components/WelcomeScreen';
+export default class App extends Component {
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyBe0wvJ4aFxGRlDUL-2XxO4-JyAo92FDfk",
+      authDomain: "tuitionvilla-fa8dd.firebaseapp.com",
+      databaseURL: "https://tuitionvilla-fa8dd.firebaseio.com",
+      projectId: "tuitionvilla-fa8dd",
+      storageBucket: "tuitionvilla-fa8dd.appspot.com",
+      messagingSenderId: "231641434744"
+    };
+    firebase.initializeApp(config);
+  }
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <Routes/>
+      </Provider>
     );
   }
 }
